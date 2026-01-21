8636be70e59750346e27563774b3335df1404712
Patrick Brown <bond_ngn@gmail.com>
2026-01-21 17:12:47 -0600

    Initial Release: Bond Node Suite v1.0

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from .bond_nodes import (
+    BatchIntPick, 
+    BatchStringPick, 
+    BondPromptArrayIterator, 
+    CartesianIndexDriverImg, 
+    CartesianIndexDriverImgPrmpt, 
+    LoadImageFromPath, 
+    RangeStepper,
+    PromptJSONSelector
+)
+
+NODE_CLASS_MAPPINGS = {
+    "BatchIntPick": BatchIntPick,
+    "BatchStringPick": BatchStringPick,
+    "BondPromptArrayIterator": BondPromptArrayIterator,
+    "CartesianIndexDriverImg": CartesianIndexDriverImg,
+    "CartesianIndexDriverImgPrmpt": CartesianIndexDriverImgPrmpt,
+    "LoadImageFromPath": LoadImageFromPath,
+    "RangeStepper": RangeStepper,
+    "PromptJSONSelector": PromptJSONSelector
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "BatchIntPick": "Bond: Batch → Int Pick",
+    "BatchStringPick": "Bond: Batch → String Pick",
+    "BondPromptArrayIterator": "Bond: Prompt Array Iterator",
+    "CartesianIndexDriverImg": "Bond: Cartesian Index Driver (Image)",
+    "CartesianIndexDriverImgPrmpt": "Bond: Cartesian Index Driver (Img + Prompt)",
+    "LoadImageFromPath": "Bond: Load Image From Path",
+    "RangeStepper": "Bond: Range Stepper",
+    "PromptJSONSelector": "Bond: Prompt JSON/TXT Selector"
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
